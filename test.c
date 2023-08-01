caadc84ee29e3b0c759c29d46c68c241fd2faf87
Awantika bijwe <30284191+AwantikaBijwe@users.noreply.github.com>
2023-08-01 16:54:58 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,1 +1,17 @@
 
+#include<stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    number1=12;
+    number2=12;
+    
+   // printf("Enter two integers: ");
+   // scanf("%d %d", &number1, &number2);
+
+    // calculate the sub
+    sum = number1 - number2;      
+    
+    printf("%d - %d = %d", number1, number2, sum);
+    printf("\ndone");
+    return 0;
