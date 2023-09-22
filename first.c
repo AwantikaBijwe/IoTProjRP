3843c4e7195fdd1d577b20c08b6848048db1154d
AwantikaBijwe <30284191+AwantikaBijwe@users.noreply.github.com>
2023-09-22 20:52:31 +0530

    done

--- first.c
+++ first.c
@@ -4,7 +4,7 @@ int main() {
     int number1, number2, sum;
     number1=22;
     number2=11;
-    
+    //second Repo
    // printf("Enter two integers: ");
    // scanf("%d %d", &number1, &number2);
 
